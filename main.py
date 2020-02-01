3202c35dbe919aebbd8a1857f5b3fbfac6549b76
abhaMore <abhay7more@gmail.com>
2020-02-01 08:30:04 +0000

    added music, sounds,  extra meteors, rotation to meteors

--- main.py
+++ main.py
@@ -1,271 +1,191 @@
+
+#music by onky Hardmoon / Arjen Schumacher
+
 import pygame
-import time
 import random
+import time
+import os
 
-pygame.init()
+width = 800
+height = 600
+FPS = 60
 
-window_width = 800		#display size
-window_height = 600
+WHITE = (255,255,255)
+BLACK = (0,0,0)
 
-ship_width = 99			#ship size
-ship_height = 75		
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
 
-a_width = 75			#asteroid size
-a_height = 75
-
-black = (0,0,0)
-white = (255,255,255)
-
-red = (150,0,0)
-bred = (255,0,0)		#b = bright
-green = (0,150,0)
-bgreen = (0,255,0)
-blue = (0,0,150)
-bblue = (0,0,255)
-
-yellow = (250,234,35)
-
-game = pygame.display.set_mode((window_width,window_height))
-pygame.display.set_caption('Space Dodging')
-
-
+pygame.init() 
+pygame.mixer.init()
+game = pygame.display.set_mode((width,height))
+pygame.display.set_caption("SPACE DOSDGING")
 clock = pygame.time.Clock()
 
-background = pygame.image.load('background.jpg')
-asteroid = pygame.image.load('asteroid.png')
-sshipimg = pygame.image.load('ship.png')
 
-pause = True
-
-def buttons(msg,x,y,w,h,ic,ac,action = None):
-    mouse = pygame.mouse.get_pos()
-    click = pygame.mouse.get_pressed()
-    if x+w > mouse[0] > x and y + h > mouse[1] > y  :		
-		pygame.draw.rect(game, ac,[x,y,w,h])
-		if click[0] ==1 and action != None:
-			if action == "cplay":
-				unpause()		
-			elif action == "play":
-				game_loop()
-			elif action == "back":
-				intro()
-			elif action == "quit":
-				pygame.quit()
-				quit()
-				
-    else :	
-		pygame.draw.rect(game, ic,[x,y,w,h])
-
-    smalltxt = pygame.font.Font("freesansbold.ttf",25)
-    txtsurf, txtrect = text_objects(msg, smalltxt,black)
-    txtrect.center = ((x + (w/2)),(y + (h/2)))
-    game.blit(txtsurf, txtrect)
+font_name =pygame.font.match_font('comic')
+def draw(surf, text, size, x, y):
+	font = pygame.font.Font(font_name, size)
+	txt_surface = font.render(text, True, GREEN)
+	txt_rect =txt_surface.get_rect()
+	txt_rect = (x,y)
+	surf.blit(txt_surface,txt_rect)
 
 
-
-def bullets(b1,b2,b3,b4,colour):
-    pygame.draw.line(game, yellow, (b1,b2),(b3,b4),1)
-
-def scorecard(count):			#scorecard count
-    font = pygame.font.Font('freesansbold.ttf', 25)
-    text = font.render("SCORE: "+str(count), True, bgreen)
-    game.blit(text,(650,0))
-
-def bground(a,b):			#background image
-	game.blit(background,(a,b))
-
-def aroid(ax,ay):			#asteroid image
-	game.blit(asteroid,(ax,ay))
+class Player(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image= pygame.transform.scale(ship,(50,38))
+		self.image.set_colorkey(BLACK)
+		self.rect = self.image.get_rect()
+		self.radius = 20
+		#pygame.draw.circle(self.image, RED,self.rect.center,self.radius, 1)
+		self.rect.centerx = width/2
+		self.rect.bottom = height - 10
+		self.speedx = 0
 	
-def sship(x,y):				#spaceship image
-    game.blit(sshipimg,(x,y))
+	def update(self):
+		self.speedx = 0
+		k = pygame.key.get_pressed()
+		if k[pygame.K_LEFT]:
+			self.speedx = -8
+		elif k[pygame.K_RIGHT]:
+			self.speedx = 8
+		self.rect.x += self.speedx
+		if self.rect.right > width:
+			self.rect.right = width
+		elif  self.rect.left < 0:
+			self.rect.left = 0
+	def shoot(self):
+		bullet = Bullet(self.rect.centerx, self.rect.top)
+		all_sprites.add(bullet)
+		bullets.add(bullet)
+		shoot_snd.play()
 
-def text_objects(text, font,color):
-    txtSurface = font.render(text, True, color)
-    return txtSurface, txtSurface.get_rect()
+class Mob(pygame.sprite.Sprite):
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image_orig = random.choice(meteor_img)
+		self.image_orig.set_colorkey(BLACK)
+		self.image = self.image_orig.copy()
+		self.rect = self.image.get_rect()
+		self.radius = int(self.rect.width * 0.85/ 2)
+		#pygame.draw.circle(self.image, RED, self.rect.center, self.radius,1)
+		self.rect.x = random.randrange(0,width - self.rect.width)
+		self.rect.y = random.randrange(-100,-40)
+		self.speedy = random.randrange(1, 8)
+		self.speedx = random.randrange(-3,3)
+		self.rot = 0	
+		self.rotspeed = random.randrange(-8,8)
+		self.t_update = pygame.time.get_ticks()	
 
-def message_display(text):
-    largeText = pygame.font.Font('freesansbold.ttf',50)
-    TextSurf, TextRect =text_objects(text, largeText, red)
-    TextRect.center = ((window_width/2),(window_height/2))
-    game.blit(TextSurf, TextRect)
+	def rotate(self):
+		now = pygame.time.get_ticks()
+		if now - self.t_update > 50 :
+			self.t_update = now
+			self.rot = (self.rot + self.rotspeed) % 360
+			new_image = pygame.transform.rotate(self.image_orig, self.rot)			
+			old_center = self.rect.center
+			self.image = new_image
+			self.rect = self.image.get_rect()
+			self.rect.center = old_center	
+	
+	def update(self):
+		self.rotate()
+		self.rect.x += self.speedx
+		self.rect.y += self.speedy
+		if self.rect.top > height + 10 or self.rect.left < -25 or self.rect.right > width + 20:
+			self.rect.x = random.randrange(width - self.rect.width)
+			self.rect.y = random.randrange(-150,-100)
+			self.speedy = random.randrange(1, 8)
 
-    pygame.display.update()
-    time.sleep(2)
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self,x,y):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.Surface((10,20))
+		self.image.fill(YELLOW)
+		self.rect = self.image.get_rect()
+		self.rect.bottom = y
+		self.rect.centerx = x
+		self.speedy = -10
+	
+	def update(self):
+		self.rect.y += self.speedy
+		if self.rect < 0 :
+			self.kill()		
+		
+bground = pygame.image.load('background.jpg').convert()
+bground_rect = bground.get_rect()
+ship = pygame.image.load('ship.png').convert()
+meteor_img = []
+meteor_list = [ 'meteor1.png','meteor2.png','meteor3.png','meteor4.png','meteor5.png',
+		'meteor6.png','meteor7.png','meteor8.png','meteor9.png','meteor10.png',
+		'meteor11.png','meteor12.png','meteor13.png','meteor14.png','meteor15.png',
+		'meteor16.png','meteor17.png','meteor18.png','meteor19.png','meteor20.png'
+		]
+for img in meteor_list :
+	meteor_img.append(pygame.image.load(img).convert())
 
-    game_loop()
+shoot_snd = pygame.mixer.Sound('shoot.wav')
+pygame.mixer.music.fadeout(2)
+explo_snd = pygame.mixer.Sound('explosion.wav')
+pygame.mixer.music.fadeout(5)
 
-def hit():
-    message_display('HIT')
 
-def crash():
-    message_display('You Crashed...RESTARTING')
+pygame.mixer.music.load('music.ogg')
+pygame.mixer.music.set_volume(1)
 
-def unpause():
-    global pause 
-    pause = False
+all_sprites = pygame.sprite.Group()
+mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 
-def paused():
-	while pause :
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()		
-			
-						
-		bground(0,0)
-		buttons("PAUSED",320,130,150,50,None,None,"")
-		buttons("CONTINUE!",320,200,150,50,blue,bblue,"cplay")
-		buttons("START!",320,270,150,50,blue,bblue,"play")
-		buttons("BACK",320,340,150,50,green,bgreen,"back")
-		buttons("EXIT",320,410,150,50,red,bred,"quit")
+player = Player()
+
+all_sprites.add(player)
+
+for i in range(8):
+	m = Mob()
+	all_sprites.add(m)
+	mobs.add(m)
+
+score = 0
+
+pygame.mixer.music.play()
+running = True
+while running :
+
+	clock.tick(FPS)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_SPACE:
+				player.shoot()
 
 		
-		pygame.display.update()
-		clock.tick(60)
-
-def intro():
-	intro = True
-	while True :
-		for event in pygame.event.get():
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_SPACE:
-					menu()
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()
-		bground(0,0)
-		Ltxt = pygame.font.Font('freesansbold.ttf',50)
-		TextSurf, TextRect =text_objects("Space Dodging", Ltxt, bblue)
-    		TextRect.center = ((window_width/2),(window_height/2))
-    		game.blit(TextSurf, TextRect)
-
-		Stxt = pygame.font.Font('freesansbold.ttf',20)
-		TextSurf, TextRect =text_objects("Press spacebar to start", Stxt, bgreen)
-		TextRect.center = ((window_width/2 + 10),(window_height/2 + 50))
-		game.blit(TextSurf, TextRect)
-		pygame.display.update()
-		clock.tick(60)
-def menu():
-	menu = True
-	while True :
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				pygame.quit()
-				quit()		
-		bground(0,0)
-		buttons("START!",320,200,150,50,blue,bblue,"play")
-		buttons("BACK",320,270,150,50,green,bgreen,"back")
-		buttons("EXIT",320,340,150,50,red,bred,"quit")
-
-		
-		pygame.display.update()
-		clock.tick(60)	
-		
-
-def game_loop():
-    
-    global pause
-    ax= random.randrange(0,window_width)
-    ay=-200
-    a_speed = 3
-
-    x = (window_width * 0.45)
-    y = (window_height * 0.8)
-
-    x_change = 0
-    y_change = 0
-
-    b_1 = x + (ship_width/2)
-    b_2 = y
-    b_3 = x + (ship_width/2)
-    b_4 = y + 10
-    score = 0
-    
-    gameExit = False
-
-    while not gameExit:
-	  bground(0,0)
-	  bullets(b_1,b_2,b_3,b_4,yellow)
-          for event in pygame.event.get():
-              if event.type==pygame.QUIT:
-                 pygame.quit()
-                 quit()
-              if event.type == pygame.KEYDOWN:
-                 if event.key == pygame.K_LEFT:
-                    x_change = -5
-                 elif event.key == pygame.K_RIGHT:
-                      x_change = 5
-                 elif event.key == pygame.K_DOWN:
-                      y_change = 5
-                 elif event.key == pygame.K_UP:
-                      y_change = -5
-		 elif event.key == pygame.K_ESCAPE:
-		      pause = True
-		      paused()
-		 elif event.key == pygame.K_SPACE:
-		      bullets(x+ ship_width/2,y,x+ ship_width/2,y+10,yellow)
-		 
-              if event.type == pygame.KEYUP:
-                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    x_change = 0
-                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    y_change = 0
-
-	  b_2 -= 3
-	  b_4 -= 3
-          x += x_change
-          y += y_change
-	  
-          
-	 
-          pygame.draw.rect(game, black, [ax,ay,a_width,a_height],1)
-	  
-	  aroid(ax,ay)
-	  ay += a_speed
-
-	  scorecard(score)
-        
-	  pygame.draw.rect(game, green, [x,y,ship_width,ship_height],1)
-
-    	  sship(x,y) 
-
-	  pygame.draw.line(game, bred, (0,0),(800,0), 4)		#boundary..
-	  pygame.draw.line(game, bred, (0,600),(800,600), 8)		#..lines
+	all_sprites.update()
+	hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+	
+	for hit in hits :
+		score += 50 - hit.radius
+		explo_snd.play()		
+		m = Mob()
+		all_sprites.add(m)
+		mobs.add(m)
 
 
-          if x + ship_width < 0 :
-             x = window_width
+	hits = pygame.sprite.spritecollide(player, mobs, False, pygame.sprite.collide_circle)
 
-          if x > window_width :
-             x= 0 - ship_width
+	if hits :
+		running = False
 
-          if y < 0 or y + ship_height > window_height:
-             crash()
-	  
-	  if ay > window_height:
-	     ay = -10
-	     ax= random.randrange(0,window_width)
-	     score += 1
+	game.fill(BLACK)
+	game.blit(bground, bground_rect)
+	all_sprites.draw(game)
+	draw(game, 'score ='+str(score),30,0,0)
+	pygame.display.flip()
 
-	  if b_4 < 0 :
-	     b_1 =x +ship_width/2
-	     b_3 =x +ship_width/2
-	     b_2=y
-	     b_4=y+10
-
-	  if b_2 < ay + a_height - 5 and b_1 > ax and b_1 < ax + a_width - 5:	     
-		hit()
-
-	  if (y < (ay + a_height) and (y > ay)) or ((y + ship_height > ay) and (y + ship_height) < (ay + a_height)):
-	     if x < ax and x > ax + a_width or x +ship_width > ax and x < ax + a_width:
-		crash()
-
-          pygame.display.update()
-          clock.tick(60)
-
-intro()
-menu()
-game_loop()
 pygame.quit()
 quit()
